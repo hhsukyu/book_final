9d5aa158de7b04dad3cf10409f424f31ea8f16cd
데브티브 <thive8564@gmail.com>
2024-01-16 12:08:11 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,4 +11,4 @@ cd $PROJECT_ROOT
 pm2 delete $APP_NAME
 pm2 start npm --name $APP_NAME -- start
 
-echo "$TIME_NOW > Deploy has been completed......"
+echo "$TIME_NOW > Deploy has been completed......."
