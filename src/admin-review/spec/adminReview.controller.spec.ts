9f8e24b109378d5f5e905f03653d482b4c8e15eb
hhsukyu <hhsukyu@gmail.com>
2024-01-18 18:46:39 +0900

    fix: spec 수정

--- src/admin-review/spec/adminReview.controller.spec.ts
+++ src/admin-review/spec/adminReview.controller.spec.ts
@@ -1,17 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminReviewController } from '../adminReview.controller';
-
+import { AdminReviewService } from '../adminReview.service';
+import { UserService } from '../../user/user.service';
+import { ConfigService } from '@nestjs/config';
 describe('AdminReviewController', () => {
   let controller: AdminReviewController;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AdminReviewController],
+      providers: [
+        AdminReviewService,
+        UserService,
+        ConfigService,
+        {
+          provide: 'AdminReviewRepository',
+          useValue: {},
+        },
+        {
+          provide: 'StoreRepository',
+          useValue: {},
+        },
+        {
+          provide: 'StoreReviewRepository',
+          useValue: {},
+        },
+        {
+          provide: 'UserRepository',
+          useValue: {},
+        },
+      ],
     }).compile();
-
     controller = module.get<AdminReviewController>(AdminReviewController);
   });
-
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
--- src/admin-review/spec/adminReview.service.spec.ts
+++ src/admin-review/spec/adminReview.service.spec.ts
@@ -1,17 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminReviewService } from '../adminReview.service';
-
+import { UserService } from '../../user/user.service';
+import { ConfigService } from '@nestjs/config';
 describe('AdminReviewService', () => {
   let service: AdminReviewService;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdminReviewService],
+      providers: [
+        AdminReviewService,
+        UserService,
+        ConfigService,
+        {
+          provide: 'AdminReviewRepository',
+          useValue: {},
+        },
+        {
+          provide: 'StoreRepository',
+          useValue: {},
+        },
+        {
+          provide: 'StoreReviewRepository',
+          useValue: {},
+        },
+        {
+          provide: 'UserRepository',
+          useValue: {},
+        },
+      ],
     }).compile();
-
     service = module.get<AdminReviewService>(AdminReviewService);
   });
-
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
