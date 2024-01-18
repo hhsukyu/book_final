6ed2fb79c775ca3c9e76941ba1280bfbf2ef146a
owensong <alslvls1995@naver.com>
2024-01-18 09:26:14 +0900

    add: book mo,co,s

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entity/user.entity';
 import { Store } from '../entity/store.entity';
 import { UserService } from '../user/user.service';
-import { Menu } from 'src/entity/menu.entity';
+import { StoreBook } from 'src/entity/store-book.entity';
+import { Menu } from '../entity/menu.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Store, Menu])],
+  imports: [TypeOrmModule.forFeature([User, Store, StoreBook, Menu])],
   providers: [StoreService, UserService],
   controllers: [StoreController],
 })
