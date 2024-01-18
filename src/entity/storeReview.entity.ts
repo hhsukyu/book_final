42683661818c697f3594bc913b7b67c850cc5b89
precipice- <configure62@gmail.com>
2024-01-18 18:52:00 +0900

    stash 목록저장용

---
+++ src/entity/storeReview.entity.ts
@@ -0,0 +1,48 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  // OneToMany,
+  PrimaryGeneratedColumn,
+  Relation,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+import { Store } from './store.entity';
+// import { AdminReview } from './adminReview.entity.ts';
+
+@Entity({
+  name: 'store_review', // 데이터베이스 테이블의 이름
+})
+export class StoreReview {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Store, (store) => store.store_reviews)
+  store: Relation<Store>;
+
+  @ManyToOne(() => User, (user) => user.store_reviews)
+  user: Relation<User>;
+
+  @Column()
+  store_id: number;
+
+  @Column()
+  user_id: number;
+
+  @Column()
+  content: string;
+
+  @Column()
+  rating: number;
+
+  // @OneToMany(() => admin_review, (admin_review) => admin_review.admin)
+  // admin_review: Relation<admin_review>[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
