3acc35d4f3310aeed43d82ea14062eb1aa62f849
Juyoung Lee <configure62@gmail.com>
2024-01-19 19:20:56 +0900

    fix: spec 변경

--- src/store-review/spec/store-review.controller.spec.ts
+++ src/store-review/spec/store-review.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StoreReviewController } from '../store-review.controller';
 import { StoreReviewService } from '../store-review.service';
+import { UserService } from '../../user/user.service';
 
 describe('StoreReviewController', () => {
   let controller: StoreReviewController;
@@ -8,7 +9,12 @@ describe('StoreReviewController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StoreReviewController],
-      providers: [StoreReviewService],
+      providers: [
+        StoreReviewService,
+        UserService,
+        { provide: 'StoreReviewRepository', useValue: {} },
+        { provide: 'UserRepository', useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<StoreReviewController>(StoreReviewController);
--- src/store-review/spec/store-review.service.spec.ts
+++ src/store-review/spec/store-review.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StoreReviewService } from '../store-review.service';
+import { UserService } from '../../user/user.service';
 
 describe('StoreReviewService', () => {
   let service: StoreReviewService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StoreReviewService],
+      controllers: [StoreReviewService],
+      providers: [
+        StoreReviewService,
+        UserService,
+        { provide: 'StoreReviewRepository', useValue: {} },
+        { provide: 'UserRepository', useValue: {} },
+      ],
     }).compile();
 
     service = module.get<StoreReviewService>(StoreReviewService);
