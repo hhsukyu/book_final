dd347995ad874cf4f10e8c725f1b7939c9f064c5
owensong <alslvls1995@naver.com>
2024-01-19 19:55:07 +0900

    test

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+// book.controller.ts
+import { Controller, Get, Query } from '@nestjs/common';
+import { BookService } from './book.service';
 
-@Controller('book')
-export class BookController {}
+@Controller('books')
+export class BookController {
+  constructor(private readonly bookService: BookService) {}
+
+  @Get()
+  async getBooks(
+    @Query('pageNo') pageNo: string,
+    @Query('viewItemCnt') viewItemCnt: string,
+  ) {
+    const books = await this.bookService.findBooks(pageNo, viewItemCnt);
+    return { books };
+  }
+}
