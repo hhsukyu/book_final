5055dc2bee4688a3da67d66177e3f6d1f44b527c
hhsukyu <hhsukyu@gmail.com>
2024-01-23 15:58:55 +0900

    feat: 영수증 유효성체크 추가작성 test o
    - 손님만 작성 가능
    - 영수증 중복 체크

--- src/receipt-auth/receipt-auth.controller.ts
+++ src/receipt-auth/receipt-auth.controller.ts
@@ -3,19 +3,24 @@ import {
   Post,
   UploadedFile,
   UseInterceptors,
+  UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ReceiptAuthService } from './receipt-auth.service';
+import { accessTokenGuard } from '../auth/guard/access-token.guard';
+import { UserId } from '../auth/decorators/userId.decorator';
 
 @Controller('receiptAuth')
 export class ReceiptAuthController {
   constructor(private readonly receiptAuthService: ReceiptAuthService) {}
 
+  @UseGuards(accessTokenGuard)
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  async postReceipt(@UploadedFile() file: Express.Multer.File) {
-    const result = await this.receiptAuthService.analyzeFile(file);
-
-    return { result };
+  async postReceipt(
+    @UploadedFile() file: Express.Multer.File,
+    @UserId() userId: number,
+  ) {
+    return await this.receiptAuthService.analyzeFile(file, userId);
   }
 }
