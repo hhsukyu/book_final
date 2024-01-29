2105ce398776a52d017666598fca7a7a66d7dcbd
gyusungjung <147012985+gyusungjung@users.noreply.github.com>
2024-01-29 19:01:31 +0900

    위시리스트,관심지점 등록삭제 최신화

--- src/map/map.module.ts
+++ src/map/map.module.ts
@@ -7,10 +7,11 @@ import { StoreService } from 'src/store/store.service';
 import { UserService } from '../user/user.service';
 import { User } from 'src/entity/user.entity';
 import { MyPage } from 'src/entity/my-page.entity';
+import { MyPageService } from 'src/my-page/my-page.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Store, User, MyPage])],
   controllers: [MapController],
-  providers: [MapService, StoreService, UserService],
+  providers: [MapService, StoreService, UserService, MyPageService],
 })
 export class MapModule {}
