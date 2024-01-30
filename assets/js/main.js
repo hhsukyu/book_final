5cd2696df4108fe0c4eba17ee6cd735a0f8e23c7
gyusungjung <147012985+gyusungjung@users.noreply.github.com>
2024-01-30 16:02:18 +0900

    aaaa

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ window.onload = function () {
 
     genrefade();
   }
-
+  loadStores();
   mainBookcard();
 };
 
---
+++ assets/js/mainstore.js
@@ -0,0 +1,27 @@
+// 매장 정보를 가져와서 표시하는 함수
+function loadStores() {
+  console.log('로드 스토어 실행됨!!!!!!!!!!!!');
+  const storeContainer = document.querySelector('.album-store');
+  storeContainer.innerHTML = ''; // 기존 내용을 비웁니다.
+  axios
+    .get('/store')
+    .then(function (response) {
+      const stores = response.data;
+      stores.forEach((store) => {
+        const storeElement = `
+          <div class="album-item">
+            <img src="${store.store_img || '기본 이미지 경로'}" alt="${store.store_name}" />
+            <div class="album-details">
+              <span class="album-title">${store.store_name}</span>
+              <span class="album-intro">${store.store_desc}</span>
+              <span class="album-location">${store.store_address}</span>
+            </div>
+          </div>
+        `;
+        storeContainer.innerHTML += storeElement;
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
