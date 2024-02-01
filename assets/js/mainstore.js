61b01496f5024aaf094bfde684b675e0c4fed38c
owensong <alslvls1995@naver.com>
2024-02-02 03:29:43 +0900

    commit for merge

--- assets/js/mainstore.js
+++ assets/js/mainstore.js
@@ -247,3 +247,47 @@ function findAdminReviewsByReview(storeid, storeReviewid) {
       throw error;
     });
 }
+
+//지점소장도서 정보
+function bookinfo(storeid) {
+  axios
+    .get(`storebook/${storeid}`)
+    .then(function (response) {
+      //   console.log(response.data);
+      const books = response.data;
+
+      books.forEach((book) => {
+        // console.log(book);
+        booklist(book);
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+const books = document.getElementById('booklist');
+function booklist(book) {
+  const bookinfo = book.book;
+  console.log(book);
+  books.innerHTML += `
+  <div id="booklistcard" class="card mb-3" >
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${bookinfo.book_image}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+        <i class="fa fa-xing" onclick="storebookdelete(${book.id})">삭제</i>
+        <div>
+          <h5 class="card-title">${bookinfo.title}</h5>
+        </div>  
+          <p class="card-text">${bookinfo.writer}</p>
+          <div id="menucardbtn">
+          <p class="card-text"><small class="text-body-secondary">${bookinfo.publisher}</small></p>
+          </div>  
+          </div>
+      </div>
+    </div>
+  </div>
+  `;
+}
