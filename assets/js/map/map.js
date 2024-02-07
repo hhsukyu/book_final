063955ceca2a22e070dc6166ab5b28e2eb3e9633
precipice- <configure62@gmail.com>
2024-02-07 23:59:10 +0900

    feat: 브라우저 기반 위치로 주변 점포 검색/주변 점포 카드 출력

--- assets/js/map/map.js
+++ assets/js/map/map.js
@@ -55,7 +55,6 @@ let allgotoPage;
 
 // onload
 async function start() {
-  const mapDiv = document.getElementById('map');
   const token = localStorage.getItem('accessToken');
 
   if (!token) {
@@ -71,11 +70,6 @@ async function start() {
 function introduce() {
   const geo = navigator.geolocation;
   if (geo) {
-    /**
-     * navigator.geolocation 은 Chrome 50 버젼 이후로 HTTP 환경에서 사용이 Deprecate 되어 HTTPS 환경에서만 사용 가능 합니다.
-     * http://localhost 에서는 사용이 가능하며, 테스트 목적으로, Chrome 의 바로가기를 만들어서 아래와 같이 설정하면 접속은 가능합니다.
-     * chrome.exe --unsafely-treat-insecure-origin-as-secure="http://example.com"
-     */
     geo.getCurrentPosition(onSuccessGeolocation, onErrorGeolocation);
   } else {
     console.log('Geolocation not supported');
@@ -90,23 +84,60 @@ async function onSuccessGeolocation(position) {
   );
   let map = new naver.maps.Map(mapDiv, {
     center: location,
-    zoom: 12,
+    zoom: 13,
   });
   let mylocation = new naver.maps.Marker({
     position: location,
     map: map,
+    icon: {
+      url: '../url/marker-pink.png',
+    },
+  });
+
+  const contentString = `
+    <div class="iw_inner">
+      <p>내 위치</p>
+    </div>`;
+
+  const infowindow = new naver.maps.InfoWindow({
+    content: contentString,
+  });
+
+  naver.maps.Event.addListener(mylocation, 'click', () => {
+    if (infowindow.getMap()) {
+      infowindow.close();
+    } else {
+      infowindow.open(map, mylocation);
+    }
   });
 
   const locationText = location.toString().split(',');
   const locationData = `${locationText[1].slice(4, -1)}, ${locationText[0].substring(5)}`;
   const marker = await storeSearch(locationData);
-  console.log(marker);
   let markers = [];
-  for (let i = 0; i < marker.length; i = i + 2) {
+  for (let i = 0; i < marker.length; i = i + 3) {
     const newmarker = new naver.maps.Marker({
       position: new naver.maps.LatLng(marker[i], marker[i + 1]),
       map: map,
     });
+
+    const contentString = `
+    <div class="iw_inner">
+      <p>${marker[i + 2]}</p>
+    </div>`;
+
+    const infowindow = new naver.maps.InfoWindow({
+      content: contentString,
+    });
+
+    naver.maps.Event.addListener(newmarker, 'click', () => {
+      if (infowindow.getMap()) {
+        infowindow.close();
+      } else {
+        infowindow.open(map, newmarker);
+      }
+    });
+
     markers.push(newmarker);
   }
 }
@@ -116,20 +147,58 @@ async function onErrorGeolocation() {
   console.log('위치 정보 권한 획득 실패');
   let map = new naver.maps.Map(mapDiv, {
     center: new naver.maps.LatLng(37.4986253, 127.0280285),
-    zoom: 12,
+    zoom: 13,
   });
   let mylocation = new naver.maps.Marker({
     position: new naver.maps.LatLng(37.4986253, 127.0280285),
     map: map,
+    icon: {
+      url: '../url/marker-pink.png',
+    },
   });
+
+  const contentString = `
+    <div class="iw_inner">
+      <p>내 위치</p>
+    </div>`;
+
+  const infowindow = new naver.maps.InfoWindow({
+    content: contentString,
+  });
+
+  naver.maps.Event.addListener(mylocation, 'click', () => {
+    if (infowindow.getMap()) {
+      infowindow.close();
+    } else {
+      infowindow.open(map, mylocation);
+    }
+  });
+
   const marker = await storeSearch('127.0280285, 37.4986253');
-  console.log(marker);
   let markers = [];
-  for (let i = 0; i < marker.length; i = i + 2) {
+  for (let i = 0; i < marker.length; i = i + 3) {
     const newmarker = new naver.maps.Marker({
       position: new naver.maps.LatLng(marker[i], marker[i + 1]),
       map: map,
     });
+
+    const contentString = `
+    <div class="iw_inner">
+      <p>${marker[i + 2]}</p>
+    </div>`;
+
+    const infowindow = new naver.maps.InfoWindow({
+      content: contentString,
+    });
+
+    naver.maps.Event.addListener(newmarker, 'click', () => {
+      if (infowindow.getMap()) {
+        infowindow.close();
+      } else {
+        infowindow.open(map, newmarker);
+      }
+    });
+
     markers.push(newmarker);
   }
 }
@@ -139,47 +208,59 @@ async function storeSearch(location) {
     const response = await axios.get(`/map/${location}`);
     const stores = response.data;
 
-    console.log(stores);
-    if (!stores) {
+    if (stores.length === 0) {
       console.log('no nearby store');
       return;
     }
 
     let latlngs = [];
+    let storesResult = [];
     stores.forEach((element) => {
-      console.log(element);
+      storesResult.push(element);
       const x = String(element.place).slice(6, -1);
       const y = x.split(' ');
       latlngs.push(y[1]); //위도 경도 순서 바꾸기
       latlngs.push(y[0]);
+      latlngs.push(element.store_name);
     });
+    searchResult(storesResult);
     return latlngs;
   } catch (error) {
     console.log(error);
   }
 }
 
-async function searchResult() {
-  axios
-    .get('/store', {})
-    .then(async function (response) {
-      const stores = response.data;
-      const pages = numPages(stores);
+async function searchResult(storesResult) {
+  const stores = storesResult;
+  if (!stores) {
+    console.log('검색 결과가 없습니다.');
+    const output = document.getElementById('output');
+    output.innerHTML += createNullElement();
+    return;
+  }
+  const pages = numPages(stores);
 
-      changePage(1); // set default page
-      await addPages(); // generate page navigation
+  changePage(1); // set default page
+  await addPages(); // generate page navigation
 
-      // reference to keep track of current page
-      let currentPage = 1;
+  // reference to keep track of current page
+  let currentPage = 1;
 
-      function numPages(cardsArray) {
-        const itemsPerPage = 8;
-        // returns the number of pages
-        return Math.ceil(cardsArray.length / itemsPerPage);
-      }
+  function createNullElement() {
+    const nullElement = `
+          <p>주변 3km 이내에 가게가 존재하지 않습니다.</p>
+    `;
+    return nullElement;
+  }
 
-      function createCardElement(store) {
-        const storeElement = `
+  function numPages(cardsArray) {
+    const itemsPerPage = 8;
+    // returns the number of pages
+    return Math.ceil(cardsArray.length / itemsPerPage);
+  }
+
+  function createCardElement(store) {
+    const storeElement = `
           <div class="album-item"onclick="storecarddetail(${store.id})">
             <img src="${store.store_img || '기본 이미지 경로'}" alt="${store.store_name}" />
             <div class="album-details">
@@ -189,61 +270,57 @@ async function searchResult() {
           </div>
         `;
 
-        return storeElement;
-      }
+    return storeElement;
+  }
 
-      function changePage(page) {
-        const output = document.getElementById('output');
-        output.innerHTML = '';
-        const itemsPerPage = 8;
+  function changePage(page) {
+    const output = document.getElementById('output');
+    output.innerHTML = '';
+    const itemsPerPage = 8;
 
-        if (page < 1) page = 1;
-        if (page > pages) page = pages;
-        output.innerHTML = '';
+    if (page < 1) page = 1;
+    if (page > pages) page = pages;
+    output.innerHTML = '';
 
-        for (
-          let i = (page - 1) * itemsPerPage;
-          i < page * itemsPerPage && i < stores.length;
-          i++
-        ) {
-          // 검색 정보 배열로 저장
+    for (
+      let i = (page - 1) * itemsPerPage;
+      i < page * itemsPerPage && i < stores.length;
+      i++
+    ) {
+      // 검색 정보 배열로 저장
 
-          const card = stores[i];
-          output.innerHTML += createCardElement(card);
-        }
-      }
+      const card = stores[i];
+      output.innerHTML += createCardElement(card);
+    }
+  }
 
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      async function addPages() {
-        const el = document.getElementById('storepages');
-        el.innerHTML = '';
-        for (let i = 1; i < pages + 1; i++) {
-          el.innerHTML += `<li><a onclick="allgotoPage(${i})">${i}</a></li>`;
-        }
-      }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async function addPages() {
+    const el = document.getElementById('storepages');
+    el.innerHTML = '';
+    for (let i = 1; i < pages + 1; i++) {
+      el.innerHTML += `<li><a onclick="allgotoPage(${i})">${i}</a></li>`;
+    }
+  }
 
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      async function nextPage() {
-        if (currentPage < pages) changePage(++currentPage);
-      }
-      allnextPage = nextPage;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async function nextPage() {
+    if (currentPage < pages) changePage(++currentPage);
+  }
+  allnextPage = nextPage;
 
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      async function prevPage() {
-        if (currentPage > 1) changePage(--currentPage);
-      }
-      allprevPage = prevPage;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async function prevPage() {
+    if (currentPage > 1) changePage(--currentPage);
+  }
+  allprevPage = prevPage;
 
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      async function gotoPage(page) {
-        currentPage = page;
-        changePage(page);
-      }
-      allgotoPage = gotoPage;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async function gotoPage(page) {
+    currentPage = page;
+    changePage(page);
+  }
+  allgotoPage = gotoPage;
 }
 
 start();
