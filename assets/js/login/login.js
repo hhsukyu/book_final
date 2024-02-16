ab9e3ca7c5014ab133640d0cd6d922b70c4dfad0
DevThive <thive8564@gmail.com>
2024-02-17 01:31:23 +0900

    accesstoken 재발급

--- assets/js/login/login.js
+++ assets/js/login/login.js
@@ -40,11 +40,6 @@ function logout() {
   localStorage.removeItem('accessToken');
   localStorage.removeItem('refreshToken');
 
-  document.cookie =
-    'accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-  document.cookie =
-    'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-
   window.location.reload();
 }
 
